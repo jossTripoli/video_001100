7fd6d9cf53687ef45f39c95963f5ad741c0e9eca
Joss <jct324@lehigh.edu>
2024-02-13 22:18:51 -0500

    speech marks gen

--- aws_lambda/main/LambdaHandler.py
+++ aws_lambda/main/LambdaHandler.py
@@ -18,6 +18,24 @@ def generateAudioUsingText(plainText,filename):
     stream = response["AudioStream"]
     bucket.put_object(Key=filename, Body=stream.read())
     
+    
+def generateSpeechMarksUsingText(plainText,filename):
+    # Generate audio using Text
+    session = Session(region_name="us-east-1")
+    polly = session.client("polly")
+    response = polly.synthesize_speech( Text=plainText,
+                                        Engine = "neural",
+                                        TextType = "text",
+                                        OutputFormat="json",
+                                        SpeechMarkTypes=["sentence","word"],
+                                        VoiceId="Danielle")
+    s3 = session.resource('s3')
+    bucket_name = "dart-store"
+    bucket = s3.Bucket(bucket_name)
+    filename = "narration/concepts/" + filename
+    stream = response["AudioStream"]
+    bucket.put_object(Key=filename, Body=stream.read())
+    
 def generateAudioUsingSSML(ssmlText,filename):
     # Generate audio using Text
     session = Session(region_name="us-east-1")
@@ -35,6 +53,22 @@ def generateAudioUsingSSML(ssmlText,filename):
     stream = response["AudioStream"]
     bucket.put_object(Key=filename, Body=stream.read())
 
+def generateSpeechMarksUsingSSML(ssmlText,filename):
+    # Generate audio using Text
+    session = Session(region_name="us-east-1")
+    polly = session.client("polly")
+    response = polly.synthesize_speech( Text=ssmlText,
+                                        Engine = "neural",
+                                        TextType = "ssml",
+                                        OutputFormat="mp3",
+                                        SpeechMarkTypes=["sentence","word"],
+                                        VoiceId="Danielle")
+    s3 = session.resource('s3')
+    bucket_name = "dart-store"
+    bucket = s3.Bucket(bucket_name)
+    filename = "narration/concepts/" + filename
+    stream = response["AudioStream"]
+    bucket.put_object(Key=filename, Body=stream.read())
 
 def lambda_handler(event, context):
 
@@ -46,5 +80,5 @@ def lambda_handler(event, context):
     
     return {
         'statusCode': 200,
-        'body': json.dumps('Successful Generation of Audio File(s) using AWS Polly')
+        'body': json.dumps('Successful Generation using AWS Polly! Your audio files are so good, even Beyoncé is taking notes.')
     }
